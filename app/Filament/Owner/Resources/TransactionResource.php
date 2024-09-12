10b3e43e3fb17efbc2a30d8b298068ed27eaced7
Souvik3798 <souvikbarua693@gmail.com>
2024-09-12 23:28:53 +0530

    price

--- app/Filament/Owner/Resources/TransactionResource.php
+++ app/Filament/Owner/Resources/TransactionResource.php
@@ -72,7 +72,7 @@ class TransactionResource extends Resource
                     })
                     ->searchable()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('cost')
+                Tables\Columns\TextColumn::make('price')
                     ->label('Price')
                     ->default(function ($record) {
                         $price = $record->price * $record->quantity;
